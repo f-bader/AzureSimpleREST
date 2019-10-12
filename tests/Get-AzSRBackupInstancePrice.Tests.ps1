5ebe8109c526f4164a9448dbb6bbac65b33d5044
Fabian Bader <f-bader@users.noreply.github.com>
2019-10-12 15:38:14 +0200

    Update to new pricing (#21)

--- tests/Get-AzSRBackupInstancePrice.Tests.ps1
+++ tests/Get-AzSRBackupInstancePrice.Tests.ps1
@@ -2,8 +2,8 @@ $FunctionName = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -Replace ".Tests
 
 Describe "$FunctionName Integration Tests" -Tags "IntegrationTests" {
     Context "Calculate correct values" {
-        It "Return 4.217 if 10 GB are used" {
-            Get-AzSRBackupInstancePrice -AllocatedDiskSpace 10 | Should -Be 4.217
+        It "Return 4.2165 if 10 GB are used" {
+            Get-AzSRBackupInstancePrice -AllocatedDiskSpace 10 | Should -Be 4.2165
         }
         It "Return 8.433 if 100 GB are used" {
             Get-AzSRBackupInstancePrice -AllocatedDiskSpace 100 | Should -Be 8.433
